59946e4277204b41c3429a423ee0b17526f6a8ff
Hana Sambora <grace.sambora@gmail.com>
2023-11-30 12:03:23 -0600

    Complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    firstWords = ", ".join(items[0:len(items)-1])
+    if len(items) > 2:
+        sentence = firstWords + ", and " + items[-1]
+    elif len(items) == 2:
+        sentence = firstWords + " and " + items[-1]
+    else:
+        return items[0]
+    
+    return sentence
